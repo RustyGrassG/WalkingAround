a8bc340f5e4972270a76272d2c08f6c16a992cff
Hunter <hentrican@gmail.com>
2025-11-01 19:57:32 -0400

    Camera Rotation

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ class main():
         #Initializes the pygame instance and sets the window caption
         pygame.init()
         pygame.display.set_caption("Walking Around")
+        pygame.event.set_grab(True)
+        pygame.mouse.set_visible(False)
+        pygame.mouse.set_relative_mode(True)
 
         #Sets up the clock for in-game time management
         self.clock = pygame.time.Clock()
@@ -18,58 +21,46 @@ class main():
         self.display = (800,600)
         pygame.display.set_mode(self.display, pylocals.DOUBLEBUF | pylocals.OPENGL)
         
-        pygame.event.set_grab(True)
+        OGL.glEnable(OGL.GL_DEPTH_TEST)
+        OGL.glEnable(OGL.GL_LIGHTING)
+        OGL.glShadeModel(OGL.GL_SMOOTH)
+        OGL.glEnable(OGL.GL_COLOR_MATERIAL)
+        OGL.glColorMaterial(OGL.GL_FRONT_AND_BACK, OGL.GL_AMBIENT_AND_DIFFUSE)
 
-        #Sets the screen background (skybox) color, and sets the perspective and position of the camera
-        OGL.glClearColor(0.1, 0.1, 0.1, 1)
-        OGLU.gluPerspective(90, (self.display[0] / self.display[1]), 0.1, 150.0)
-        OGL.glTranslatef(0.0, 0.0, 0.0)
+        OGL.glEnable(OGL.GL_LIGHT0)
+        OGL.glLightfv(OGL.GL_LIGHT0, OGL.GL_AMBIENT, [0.5, 0.5, 0.5, 1])
+        OGL.glLightfv(OGL.GL_LIGHT0, OGL.GL_AMBIENT, [1.0, 1.0, 1.0, 1])
 
-        #Sets up the player stats like speed, jump height, and more!
+        cube_1 = drawObject.Cube(pos=(3, 10, -5), color=(.5,.3,.5))
+        cube_2 = drawObject.Cube(pos=(0, -2, -5), color=(.1,.7,.0))
+        cube_3 = drawObject.Cube(pos=(3, 4, -5), color=(.2,.3,.5))
+        cube_4 = drawObject.Cube(pos=(-10.0, 0, -5.0), color=(.3,.2,.3))
+        self.objects = [
+            cube_1,
+            cube_2,
+            cube_3,
+            cube_4
+        ]
+
+        OGL.glMatrixMode(OGL.GL_PROJECTION)
+        OGLU.gluPerspective(45, (self.display[0]/self.display[1]), 0.1, 50.0)
+
+        OGL.glMatrixMode(OGL.GL_MODELVIEW)
+        OGLU.gluLookAt(0, -8, 0, 0, 0, 0, 0, 0, 1)
+        self.viewMatrix = OGL.glGetFloatv(OGL.GL_MODELVIEW_MATRIX)
+        OGL.glLoadIdentity()
+
+        self.displayCenter = [self.display[0] // 2, self.display[1] // 2]
+        self.mouseMove = [0,0]
+        pygame.mouse.set_pos(self.displayCenter)
+
+        self.up_down_angle = 0.0
+
+        self.player_pos = [0.0, 0.0, 0.0]
         self.player_stats = {
             "walk_speed" : 10.0,
-            "camera_sensitivity" : 0.01
+            "camera_sensitivity" : 2.0
         }
-
-        #Sets up the player position(x,y,z)
-        self.player_height : float = -2.0
-        self.player_pos = [0.0,self.player_height,0.0]
-        #Use only X and Z to rotate the player
-        self.player_yaw = 0.0
-        self.player_pitch = 0.0
-        self.max_pitch = 89.0
-        #self.update_camera(0,0)
-
-        #Stores every object to maintain persistance!
-        self.objects = []
-        new_cube = drawObject.Cube(color=(1,.5,.5), pos=(0,0,-20))
-        self.objects.append(new_cube)
-        new_cube2 = drawObject.Cube(color=(.3,.5,.2), pos=(4,0,-15))
-        self.objects.append(new_cube2)
-        #new_sphere = drawObject.Sphere()
-        #self.objects.append(new_sphere)
-    
-    #intakes the mouse movement per tick and adds it to camera pitch/yaw
-    #Takes the current pitch/yaw and adds it to the camera rotation
-    #Currently not working... I want this to not use the glu lookat function cause it interferes with my player movement i think
-    def update_camera(self, amount_x, amount_y):
-        player_pos = self.player_pos.copy()
-        print(str(amount_x)+ "," + str(amount_y))
-        self.player_yaw += math.radians(amount_x)
-        self.player_pitch += math.radians(amount_y)
-        self.player_pitch = max(-self.max_pitch, min(self.max_pitch, self.player_pitch))
-        forward_x = math.cos(self.player_yaw) * math.cos(self.player_pitch)
-        forward_y = math.sin(self.player_pitch)
-        forward_z = -math.sin(self.player_yaw) * math.cos(self.player_pitch)
-        forward_vector = [forward_x, forward_y, forward_z]
-        target = [
-            player_pos[0] + forward_vector[0],
-            player_pos[1] + forward_vector[1],
-            player_pos[2] + forward_vector[2]
-        ]
-        OGLU.gluLookAt(player_pos[0], player_pos[1], player_pos[2],
-                        target[0], target[1], target[2],
-                        0,1,0)
         
 
     def run(self):
@@ -84,23 +75,42 @@ class main():
                         pygame.quit()
                         return
             
-            pygame.mouse.set_pos(self.display[0] // 2, self.display[1] // 2)
+            key_presses = pygame.key.get_pressed()
             dx, dy = pygame.mouse.get_rel()
-            dx *= self.player_stats["camera_sensitivity"] * dt
-            dy *= self.player_stats["camera_sensitivity"] * dt
-            if dx or dy:
-                self.update_camera(dx, dy)
             
-                
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_a]:
+            OGL.glLoadIdentity()
+
+            if dx or dy:
+                dx *= self.player_stats["camera_sensitivity"] * dt
+                dy *= self.player_stats["camera_sensitivity"] * dt
+                pygame.mouse.set_pos(self.displayCenter)
+
+            OGL.glLoadIdentity()
+
+            self.up_down_angle += dy
+            OGL.glRotatef(self.up_down_angle, 1.0, 0.0, 0.0)
+
+            OGL.glPushMatrix()
+            OGL.glLoadIdentity()
+
+            if key_presses[pygame.K_a]:
                 self.player_pos[0] += self.player_stats["walk_speed"] * dt
-            if keys[pygame.K_d]:
+            if key_presses[pygame.K_d]:
                 self.player_pos[0] -= self.player_stats["walk_speed"] * dt
-            if keys[pygame.K_w]:
-                self.player_pos[2] += self.player_stats["walk_speed"] * dt
-            if keys[pygame.K_s]:
-                self.player_pos[2] -= self.player_stats["walk_speed"] * dt
+            if key_presses[pygame.K_w]:
+                self.player_pos[1] -= self.player_stats["walk_speed"] * dt
+            if key_presses[pygame.K_s]:
+                self.player_pos[1] += self.player_stats["walk_speed"] * dt
+
+            OGL.glRotatef(dx, 0.0, 1.0, 0.0)
+
+            OGL.glMultMatrixf(self.viewMatrix)
+            self.viewMatrix = OGL.glGetFloatv(OGL.GL_MODELVIEW_MATRIX)
+
+            OGL.glPopMatrix()
+            OGL.glMultMatrixf(self.viewMatrix)
+
+            OGL.glLightfv(OGL.GL_LIGHT0, OGL.GL_POSITION, [1,-1,1,0])
             
             #refreshes the screen each tick
             OGL.glClear(OGL.GL_COLOR_BUFFER_BIT | OGL.GL_DEPTH_BUFFER_BIT)

a459bb179fabb89845a5cfbf6527b5b32f31a0d5
Hunter <hentrican@gmail.com>
2025-11-03 20:59:55 -0500

    Added a cube counter and a way to manually add cubes while running the app. (Press space)

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -1,1 +1,10 @@
 #Handles most utility functions like loading saves, or other assets into the game
+import pygame
+from OpenGL.GL import  *
+from OpenGL.GLU import *
+import numpy as np
+
+#Turns a regular pygame surface into an OpenGL texture
+def alpha_surf_to_gl(surface):
+    glBindTexture(GL_TEXTURE_2D, surface)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface.width(), surface.height(), GL_RGBA, GL_UNSIGNED_BYTE, None)
--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import Scripts
 import pygame
 import math
 from random import uniform
-import OpenGL.GL as OGL
-import OpenGL.GLU as OGLU
+from OpenGL.GL import  *
+from OpenGL.GLU import *
 import Scripts.drawObject as drawObject, Scripts.entities as entities
 from pygame import locals as pylocals
+import numpy as np
 
 
 
 class main():
     def __init__(self):
+        self.debug = True
         #Initializes the pygame instance and sets the window caption
         pygame.init()
+        pygame.font.init()
         pygame.display.set_caption("Walking Around")
         pygame.event.set_grab(True)
         pygame.mouse.set_visible(False)
@@ -21,38 +24,62 @@ class main():
 
         #Sets up the clock for in-game time management
         self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font(pygame.font.get_default_font(), 50)
+        self.text = "None"
+        self.text = self.font.render("Cubes: " , False, (255, 255, 255))
 
         #Sets display window size and buffering - Allows OpenGL to render properly to the screen
         self.display = (800,600)
-        pygame.display.set_mode(self.display, pylocals.DOUBLEBUF | pylocals.OPENGL)
-        
-        OGL.glEnable(OGL.GL_DEPTH_TEST)
-        OGL.glEnable(OGL.GL_LIGHTING)
-        OGL.glShadeModel(OGL.GL_SMOOTH)
-        OGL.glEnable(OGL.GL_COLOR_MATERIAL)
-        OGL.glColorMaterial(OGL.GL_FRONT_AND_BACK, OGL.GL_AMBIENT_AND_DIFFUSE)
+        self.screen = pygame.display.set_mode(self.display, pylocals.DOUBLEBUF | pylocals.OPENGL)
+        self.ui_layer = pygame.Surface(self.display, pygame.SRCALPHA)
+        self.ui_layer.fill((0,0,0, 0))
+        self.ui_layer.blit(self.text, (10,10))
+        self.ui_texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, self.ui_texture)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, self.display[0], self.display[1], 0, GL_RGBA, GL_UNSIGNED_BYTE, None)
 
-        OGL.glEnable(OGL.GL_LIGHT0)
-        OGL.glLightfv(OGL.GL_LIGHT0, OGL.GL_AMBIENT, [0.5, 0.5, 0.5, 1])
-        OGL.glLightfv(OGL.GL_LIGHT0, OGL.GL_AMBIENT, [1.0, 1.0, 1.0, 1])
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glBindTexture(GL_TEXTURE_2D, 0)
+        
+
+        #Setting the pygame font for writting things on screen
+        #print(pygame.font.get_fonts())
+        
+        
+        
+        
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_LIGHTING)
+        glShadeModel(GL_SMOOTH)
+        glEnable(GL_COLOR_MATERIAL)
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+
+        glEnable(GL_LIGHT0)
+        glLightfv(GL_LIGHT0, GL_AMBIENT, [0.5, 0.5, 0.5, 1])
+        glLightfv(GL_LIGHT0, GL_AMBIENT, [1.0, 1.0, 1.0, 1])
 
         self.objects = []
 
-        for i in range(400):
+        if self.debug:
+            self.avg_fps = 0
+            self.fps_tick = 0
+
+        for i in range(0):
             x = uniform(-100.0, 100.0)
             y = uniform(-100.0, 100.0)
             z = uniform(-100.0, 100.0)
             #Set Cube stats here! Only size, position, and color works
-            self.objects.append(drawObject.Cube(pos = (x, y, z), color= (x/(x+y+z), y/(x+y+z), z/(x+y+z)), size = .25))
-        
+            self.objects.append(drawObject.Cube(pos = (x, y, z), color= (x/(x+y+z), y/(x+y+z), z/(x+y+z)), size = uniform(0.25, 2.5)))
+        self.update_cube_count()
 
-        OGL.glMatrixMode(OGL.GL_PROJECTION)
-        OGLU.gluPerspective(45, (self.display[0]/self.display[1]), 0.1, 150.0)
+        glMatrixMode(GL_PROJECTION)
+        gluPerspective(45, (self.display[0]/self.display[1]), 0.1, 150.0)
 
-        OGL.glMatrixMode(OGL.GL_MODELVIEW)
-        OGLU.gluLookAt(0, -8, 0, 0, 0, 0, 0, 0, 1)
-        self.viewMatrix = OGL.glGetFloatv(OGL.GL_MODELVIEW_MATRIX)
-        OGL.glLoadIdentity()
+        glMatrixMode(GL_MODELVIEW)
+        gluLookAt(0, -8, 0, 0, 0, 0, 0, 0, 1)
+        self.viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+        glLoadIdentity()
 
         #The center of the display window overlay
         self.displayCenter = [self.display[0] // 2, self.display[1] // 2]
@@ -65,11 +92,78 @@ class main():
         self.player = entities.Player()
         
 
+    def update_cube_count(self):
+        self.ui_layer.fill((0,0,0,0))
+        self.text = self.font.render("Cubes: " + str(len(self.objects)), True, (255, 255, 255))
+        self.ui_layer.blit(self.text, (10,10))
+        #self.text_data = pygame.image.tobytes(self.text, "RGBA", True)
+        
+    def surf_to_texture(self, surface, texID):
+        width, height = surface.get_size()
+        surface_array = pygame.surfarray.pixels3d(surface)
+        surface_array = np.flipud(surface_array)
+        rgb_surface = surface_array.swapaxes(0, 1).astype(np.uint8).tobytes()
+        flipped_surf = pygame.transform.flip(surface, False, True)
+        texture_data = pygame.image.tobytes(flipped_surf, "RGBA", True)
+
+        glBindTexture(GL_TEXTURE_2D, texID)
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, texture_data)
+        glBindTexture(GL_TEXTURE_2D, 0)
+
+    def draw_ui_overlay(self):
+        glMatrixMode(GL_PROJECTION)
+        glPushMatrix()
+        glLoadIdentity()
+        glOrtho(0, self.display[0], self.display[1], 0, -1, 1)
+
+        glMatrixMode(GL_MODELVIEW)
+        glPushMatrix()
+        glLoadIdentity()
+
+        glDisable(GL_DEPTH_TEST)
+        glDisable(GL_LIGHTING)
+        glColor3f(1.0, 1.0, 1.0)
+
+        glEnable(GL_TEXTURE_2D)
+        glBindTexture(GL_TEXTURE_2D, self.ui_texture)
+
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        # Draw a rectangle in screen space
+        glBegin(GL_QUADS)
+        glTexCoord2f(0, 0); glVertex2f(0, 0)
+        glTexCoord2f(1, 0); glVertex2f(self.display[0], 0)
+        glTexCoord2f(1, 1); glVertex2f(self.display[0], self.display[1])
+        glTexCoord2f(0, 1); glVertex2f(0, self.display[1])
+        glEnd()
+
+        glBindTexture(GL_TEXTURE_2D, 0)
+        glDisable(GL_TEXTURE_2D)
+        glDisable(GL_BLEND)
+
+        glEnable(GL_LIGHTING)
+        glEnable(GL_DEPTH_TEST)
+
+        # restore projection + modelview
+        glPopMatrix()
+        glMatrixMode(GL_PROJECTION)
+        glPopMatrix()
+        glMatrixMode(GL_MODELVIEW)
+
     def run(self):
         while True:
             #Delta time... dont think this is how it works IRL, will probably have to use FPS to make it work
             dt = self.clock.tick(60) / 1000
-            print(self.clock.get_fps())
+            if self.debug:
+                self.avg_fps += self.clock.get_fps()
+                self.fps_tick += 1
+                avg_fps = self.avg_fps / self.fps_tick
+                print("Average FPS: " + str(avg_fps))
+                if self.fps_tick >= 60:
+                    self.avg_fps = avg_fps
+                    self.fps_tick = 1
+                    
+                
             for event in pygame.event.get():
                 if event.type == pylocals.QUIT:
                     pygame.quit()
@@ -78,11 +172,18 @@ class main():
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                         return
-            
+                    if event.key == pygame.K_SPACE:
+                        x = uniform(-100.0, 100.0)
+                        y = uniform(-100.0, 100.0)
+                        z = uniform(-100.0, 100.0)
+                        #Set Cube stats here! Only size, position, and color works
+                        self.objects.append(drawObject.Cube(pos = (x, y, z), color= (x/(x+y+z), y/(x+y+z), z/(x+y+z)), size = uniform(0.25, 2.5)))
+                        self.update_cube_count()
+
             key_presses = pygame.key.get_pressed()
             dx, dy = pygame.mouse.get_rel()
             
-            OGL.glLoadIdentity()
+            glLoadIdentity()
 
             #if dx or dy:
                 #sensitivity = self.player.stats["mouse_sensitivity"]
@@ -91,28 +192,35 @@ class main():
                 
                 
 
-            OGL.glLoadIdentity()
+            glLoadIdentity()
             self.player.update_camera(dx, dy, self.displayCenter, dt)
 
-            OGL.glPushMatrix()
-            OGL.glLoadIdentity()
+            glPushMatrix()
+            glLoadIdentity()
 
             self.player.update_pos(key_presses, dt)
 
-            OGL.glMultMatrixf(self.viewMatrix)
-            self.viewMatrix = OGL.glGetFloatv(OGL.GL_MODELVIEW_MATRIX)
+            glMultMatrixf(self.viewMatrix)
+            self.viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
 
-            OGL.glPopMatrix()
-            OGL.glMultMatrixf(self.viewMatrix)
+            glPopMatrix()
+            glMultMatrixf(self.viewMatrix)
 
-            OGL.glLightfv(OGL.GL_LIGHT0, OGL.GL_POSITION, [1,-1,1,0])
+            glLightfv(GL_LIGHT0, GL_POSITION, [1,-1,1,0])
+            
             
             #refreshes the screen each tick
-            OGL.glClear(OGL.GL_COLOR_BUFFER_BIT | OGL.GL_DEPTH_BUFFER_BIT)
-            OGL.glBegin(OGL.GL_QUADS)
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+            self.surf_to_texture(self.ui_layer, self.ui_texture)
+            glBegin(GL_QUADS)
             for game_object in self.objects:
                 game_object.draw(self.player.pos)
-            OGL.glEnd()
+            glEnd()
+            glEnable(GL_TEXTURE_2D)
+            glBindTexture(GL_TEXTURE_2D, 1)
+            #pygame.display.get_surface().blit(self.text, self.displayCenter)
+            #self.screen.blit(self.text, self.displayCenter)
+            self.draw_ui_overlay()
             pygame.display.flip()
             
 
